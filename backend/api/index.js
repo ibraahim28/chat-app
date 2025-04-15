7f6c57cd7f4f141905c97d7a01c5c3985ac47262
ibraahim28 <ibraahimimran23@gmail.com>
2025-04-15 15:47:00 +0500

    deploying

---
+++ backend/api/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { app, server } = require("../src/lib/socket");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("../src/lib/db");
+const authRouter = require("../src/routers/authRouter");
+const userRouter = require("../src/routers/userRouter");
+const messageRouter = require("../src/routers/messageRouter");
+const protectRoute = require("../src/middlewares/protectRoute");
+
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
+app.use("/auth", authRouter);
+app.use("/users", protectRoute, userRouter);
+app.use("/messages", protectRoute, messageRouter);
+
+// const PORT = process.env.PORT || 5000;
+// server.listen(PORT, () => {
+//   console.log("App running in port: " + PORT);
+//   connectDB();
+// });
+
+module.exports = app;
