215a33b5488d7fbdb24a4bfdda90be40c728f768
ibraahim28 <ibraahimimran23@gmail.com>
2025-04-17 15:07:06 +0500

    cors issue resolving

--- backend/api/index.js
+++ backend/api/index.js
@@ -15,6 +15,15 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://chatmore-ibra.netlify.app");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 app.use("/auth", authRouter);
