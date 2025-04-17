3c12e3957989987af3c276b6ba27b231cc4a6b4a
ibraahim28 <ibraahimimran23@gmail.com>
2025-04-17 20:33:07 +0500

    fixed the wrong path in backend start script

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const BASE_URL = "https://chat-app-backend-olive-two.vercel.app";
+export const BASE_URL = "https://chat-app-backend-olive-two.vercel.app";  
   console.log("BASE_URL==========", BASE_URL)
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
